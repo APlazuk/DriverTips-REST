1db74289365fbec7eac831b98a63f875d7448486
Aleksandra Plazuk <aleksandraplazuk@o2.pl>
2020-04-28 16:42:17 +0200

    QuestionService: modify createNew method

--- src/main/java/pl/coderslab/drivertips/services/impl/DefaultQuestionService.java
+++ src/main/java/pl/coderslab/drivertips/services/impl/DefaultQuestionService.java
@@ -1,30 +1,37 @@
 package pl.coderslab.drivertips.services.impl;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.drivertips.exceptions.MultimediaNotFoundException;
 import pl.coderslab.drivertips.exceptions.QuestionNotFoundException;
+import pl.coderslab.drivertips.model.Multimedia;
 import pl.coderslab.drivertips.model.Question;
 import pl.coderslab.drivertips.model.Training;
+import pl.coderslab.drivertips.repositories.MultimediaRepository;
 import pl.coderslab.drivertips.repositories.QuestionRepository;
 import pl.coderslab.drivertips.services.QuestionService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Optional;
 
 @Service
 public class DefaultQuestionService implements QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final MultimediaRepository multimediaRepository;
 
-    public DefaultQuestionService(QuestionRepository questionRepository) {
+    public DefaultQuestionService(QuestionRepository questionRepository, MultimediaRepository multimediaRepository) {
         this.questionRepository = questionRepository;
+        this.multimediaRepository = multimediaRepository;
     }
 
     @Override
-    public List <Question> getQuestionByTrainingId(Long tipId, Long trainingId) {
-        Optional<List<Question>> questionByTipIdAndTrainingId = questionRepository.getQuestionsByTipIdAndTrainingId(tipId,trainingId);
+    public List<Question> getQuestionByTrainingId(Long tipId, Long trainingId) {
+        Optional<List<Question>> questionByTipIdAndTrainingId = questionRepository.getQuestionsByTipIdAndTrainingId(tipId, trainingId);
 
-        if (questionByTipIdAndTrainingId.isEmpty()){
-            throw new QuestionNotFoundException(String.format("Pytanie dla danego id porady: %s oraz treningu: %s nie istnieje", tipId,trainingId));
+        if (questionByTipIdAndTrainingId.isEmpty()) {
+            throw new QuestionNotFoundException(String.format("Pytanie dla danego id porady: %s oraz treningu: %s nie istnieje", tipId, trainingId));
         }
 
         return questionByTipIdAndTrainingId.get();
@@ -32,9 +39,25 @@ public class DefaultQuestionService implements QuestionService {
 
     @Override
     public Question createNewQuestion(Question question, Training training) {
+        List<Multimedia> multimedia = question.getMultimedia();
+        ListIterator<Multimedia> multimediaListIterator = multimedia.listIterator();
+
+        Optional<Multimedia> mediaFromDB = Optional.empty();
+        if (multimediaListIterator.hasNext()) {
+            Multimedia next = multimediaListIterator.next();
+            mediaFromDB = multimediaRepository.findMultimediaById(next.getId());
+        }
+
+        if (mediaFromDB.isEmpty()) {
+            throw new MultimediaNotFoundException(String.format("Medium o danym id: '%s' nie zostało znalezione", mediaFromDB.get().getId()));
+        }
+
+        List<Multimedia> media = new ArrayList<>();
+        media.add(mediaFromDB.get());
+
+        question.setMultimedia(media);
         question.setTraining(training);
 
         return questionRepository.save(question);
-
     }
 }
