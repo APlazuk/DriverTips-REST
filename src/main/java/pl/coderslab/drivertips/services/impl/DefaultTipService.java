bfce41f8284f043b96c1216a10d9cdbe57853e81
Aleksandra Plazuk <aleksandraplazuk@o2.pl>
2020-05-04 19:31:50 +0200

    TipService: modify CreateNew method

--- src/main/java/pl/coderslab/drivertips/services/impl/DefaultTipService.java
+++ src/main/java/pl/coderslab/drivertips/services/impl/DefaultTipService.java
@@ -3,14 +3,19 @@ package pl.coderslab.drivertips.services.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.coderslab.drivertips.model.Multimedia;
+import pl.coderslab.drivertips.model.Tag;
 import pl.coderslab.drivertips.model.Tip;
 import pl.coderslab.drivertips.exceptions.TipNotFoundException;
+import pl.coderslab.drivertips.repositories.TagRepository;
 import pl.coderslab.drivertips.repositories.TipRepository;
 import pl.coderslab.drivertips.services.MultimediaService;
+import pl.coderslab.drivertips.services.TagService;
 import pl.coderslab.drivertips.services.TipService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -18,10 +23,12 @@ public class DefaultTipService implements TipService {
 
     private final TipRepository tipRepository;
     private final MultimediaService multimediaService;
+    private final TagService tagService;
 
-    public DefaultTipService(TipRepository tipRepository, MultimediaService multimediaService) {
+    public DefaultTipService(TipRepository tipRepository, MultimediaService multimediaService, TagService tagService) {
         this.tipRepository = tipRepository;
         this.multimediaService = multimediaService;
+        this.tagService = tagService;
     }
 
     @Override
@@ -61,6 +68,11 @@ public class DefaultTipService implements TipService {
         List<Multimedia> media = multimediaService.getMultimedia(multimedia);
 
         tip.setMultimedia(media);
+
+        Set<Tag> tags = tip.getTags();
+        Set<Tag> tagSet = tags.stream().map(tag -> tagService.createNewTag(tag,tip)).collect(Collectors.toSet());
+
+        tip.setTags(tagSet);
 
         return tipRepository.save(tip);
     }
