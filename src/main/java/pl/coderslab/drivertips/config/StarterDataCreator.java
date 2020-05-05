ab349bbb2d6a3387bc8e1d3f0d4c4e9215bb82ce
Aleksandra Plazuk <aleksandraplazuk@o2.pl>
2020-05-05 20:20:25 +0200

    Heroku Configuration

--- src/main/java/pl/coderslab/drivertips/config/StarterDataCreator.java
+++ src/main/java/pl/coderslab/drivertips/config/StarterDataCreator.java
@@ -4,9 +4,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import pl.coderslab.drivertips.model.User;
+import pl.coderslab.drivertips.model.*;
 import pl.coderslab.drivertips.repositories.UserRepository;
 import pl.coderslab.drivertips.services.UserService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @Component
@@ -25,6 +30,7 @@ public class StarterDataCreator implements ApplicationRunner {
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
         createUser();
+        createTipWithFullTraining();
     }
 
     private void createUser() {
@@ -35,5 +41,52 @@ public class StarterDataCreator implements ApplicationRunner {
         if (!userRepository.existsUserByUsername(user.getUsername())){
             userService.saveUser(user);
         }
+    }
+
+    private void createTipWithFullTraining(){
+
+        Answer answer = new Answer();
+        answer.setText("long-distance truckers");
+        answer.setCorrect(true);
+
+        Answer answer1 = new Answer();
+        answer.setText("delivery people");
+        answer.setCorrect(false);
+
+        Question question = new Question();
+        question.setText("Drowsy driving is a major hazard, especially for:");
+        question.setPoints(5);
+
+        List<Answer> answers = new ArrayList<>();
+        answers.add(answer);
+        answers.add(answer1);
+        question.setAnswers(answers);
+
+
+        Training training = new Training();
+        training.setTitle("Stay Alert");
+
+        Tag tag = new Tag();
+        tag.setName("Pay Attention");
+
+        List<Question> questions = new ArrayList<>();
+        questions.add(question);
+        training.setQuestions(questions);
+
+        Tip tip = new Tip();
+        tip.setTitle("Stay Alert");
+        tip.setDescription("Actively pay attention to your actions and those of the drivers around you when you are driving");
+
+        Set<Tag> tags = new HashSet<>();
+        tags.add(tag);
+        tip.setTags(tags);
+
+        tip.setTraining(training);
+
+
+        training.setTip(tip);
+        question.setTraining(training);
+        answer.setQuestion(question);
+
     }
 }
