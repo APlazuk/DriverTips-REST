8bb5863f53cb9203607cac52c84d828356c435ee
Aleksandra Plazuk <aleksandraplazuk@o2.pl>
2020-05-05 19:51:38 +0200

    Heroku Configuration

--- src/main/java/pl/coderslab/drivertips/DriverTipsApplication.java
+++ src/main/java/pl/coderslab/drivertips/DriverTipsApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "pl.coderslab.drivertips.*")
-public class DriverTipsApplication {
+public class DriverTipsApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers (ViewControllerRegistry registry) {
+		RedirectViewControllerRegistration r =
+				registry.addRedirectViewController("/", "/swagger-ui.html");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DriverTipsApplication.class, args);
